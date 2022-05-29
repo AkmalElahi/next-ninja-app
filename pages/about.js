4c20da8503474d1eec4d7ee355e6bb06347142a8
akmalelahi <akmalelahishaikh@gmail.com>
2022-05-30 00:12:39 +0500

    next.js starter app

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head >
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in massa eget sapien vehicula vehicula. Etiam at laoreet massa. Quisque sed tortor rutrum, viverra nisi in, efficitur justo. In et massa nec orci lobortis consequat quis eget leo.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in massa eget sapien vehicula vehicula. Etiam at laoreet massa. Quisque sed tortor rutrum, viverra nisi in, efficitur justo. In et massa nec orci lobortis consequat quis eget leo.</p>
+      </div>
+    </>
+  );
+}
+
+export default About;
